f3eac76d4502fc593852cf64961586cbf6808fd0
Andrew Palacci <andrewpalacci1@gmail.com>
2024-12-10 23:51:38 -0500

    automated setup not working + removed other readme

--- setup.sh
+++ setup.sh
@@ -1,49 +1,21 @@
-#!/bin/bash
+# Perform these steps manually:
 
-# Install Podman on EC2, and other useful dependencies
-sudo apt update && sudo apt install -y podman
-pip install numpy tqdm
+# install podman on ec2
+sudo apt install podman
 
-# Pull Fedora base image if not already available
-podman pull fedora
+# install dependencies on container
+podman run -it fedora bash
+dnf update -y
+dnf install -y strace htop python3 python3-pip
+exit
 
-# Install dependencies in the container
-podman run --name fedora_temp fedora bash -c "
-    dnf update -y &&
-    dnf install -y strace htop python3 python3-pip psutil
-"
+# commit it to an image after install 
+podman container ls -a
+podman commit <container_id> straceimg
 
-# Get the most recently created container ID and commit it to an image
-container_id=$(podman ps -a --format "{{.ID}}" --sort created | tail -n 1)
-podman commit "$container_id" straceimg
-
-# Run the malware-isolated container with the created image
-# NOTE: make sure to run from top-level directory
-# podman run -it \
-#     --cap-add=SYS_PTRACE \
-#     --network none \
-#     -v $(pwd):/mnt/ \
-#     straceimg bash
-
-
-# Old version below in case you want to do this manually instead:
-
-# # install podman on ec2
-# sudo apt install podman
-
-# # install dependencies on container
-# podman run -it fedora bash
-# dnf update -y
-# dnf install -y strace htop python3 python3-pip
-# exit
-
-# # commit it to an image after install 
-# podman container ls -a
-# podman commit <container_id> straceimg
-
-# # get malware isolated container container
-# podman run -it \
-#     --cap-add=SYS_PTRACE \
-#     --network none \
-#     -v $(pwd):/mnt/ \
-#     straceimg bash
+# get malware isolated container container
+podman run -it \
+    --cap-add=SYS_PTRACE \
+    --network none \
+    -v $(pwd):/mnt/ \
+    straceimg bash
