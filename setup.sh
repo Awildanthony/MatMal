6d47565636f25d095dd0c62947276bebe9834c21
Andrew Palacci <andrewpalacci1@gmail.com>
2024-12-11 14:32:53 -0500

    finished readme, polished setup, deleted unnecessary driver

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@
 sudo apt install podman python3 python3-pip
 pip install numpy tqdm
 
-# install dependencies on container
+# install dependencies in container
 podman run -it fedora bash
 dnf update -y
 dnf install -y strace htop python3 python3-pip
 pip install psutil
 exit
 
-# commit it to an image after install 
+# commit container to an image after install 
 podman container ls -a
 podman commit <container_id> straceimg
 
-# get malware isolated container container
+# [OPTIONAL] open an isolated container to test any malware individually
 podman run -it \
     --cap-add=SYS_PTRACE \
     --network none \
